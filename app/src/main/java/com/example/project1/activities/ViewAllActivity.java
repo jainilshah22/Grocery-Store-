6e22311e6bd89760dad90f73223449bfb5346110
jainilshah22 <jainilshah2001@gmail.com>
2023-03-15 13:49:35 +0530

    hello

---
+++ app/src/main/java/com/example/project1/activities/ViewAllActivity.java
@@ -0,0 +1,16 @@
+package com.example.project1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project1.R;
+
+public class ViewAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+    }
+}
